71ac18370b1afbfe24964b50169d571ef4ec3f61
Rizmari Versfeld <rizmari@praekelt.com>
2012-05-29 11:20:53 +0200

    add stubs

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-insight',
+    version='0.0.1',
+    description='Records and displays registration origin stats.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/jmbo-foundry',
+    packages = find_packages(),
+    install_requires = [
+    ],
+    include_package_data=True,
+    tests_require=[
+        'django-setuptest',
+    ],
+    test_suite="setuptest.SetupTestSuite",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
