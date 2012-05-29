d0b4d4fb9b7661c8d07927794cdc194d1681b3f1
Rizmari Versfeld <rizmari@praekelt.com>
2012-05-29 12:10:26 +0200

    function to generate 7-character origin code

--- insight/models.py
+++ insight/models.py
@@ -1,12 +1,21 @@
+import uuid
+
 from django.db import models
 from django.db.models import F
 
 
 class Origin(models.Model):
-    code = models.CharField(unique=True, max_length=20)
+    code = models.CharField(unique=True, max_length=7, editable=False)
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
-    number_of_registrations = models.IntegerField()
+    number_of_registrations = models.IntegerField(editable=False)
+    
+    def generate_code():
+        while True:
+            # we expect to have few origin objects, so this is ok
+            cde = uuid.uuid4().hex[:7]
+            if not Origin.objects.filter(code=cde).exists():
+                return cde
 
 
 class Registration(models.Model):
