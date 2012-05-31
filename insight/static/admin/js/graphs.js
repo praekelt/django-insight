92f537e51ee0f61f5b9dd6f99dbf1715b8a85b05
Rizmari Versfeld <rizmari@praekelt.com>
2012-05-31 13:03:34 +0200

    pie chart done

--- insight/static/admin/js/graphs.js
+++ insight/static/admin/js/graphs.js
@@ -1,8 +1,6 @@
 var IGraphs = IGraphs || {}; // namespace for Insight Graphs
 
-/*
- * Initialize global IGraphs variables here
- */
+// Initialize global IGraphs variables here
 IGraphs.init = function() {
     IGraphs.container = d3.select("#graphs");
 };
@@ -10,10 +8,21 @@ IGraphs.init = function() {
 /*
  * Base Graph object
  */
-IGraphs.Graph = function(title) {
+IGraphs.Graph = function(title, width, height) {
     this.title = title;
-    this.layout = undefined; // defined in children
-    
+    this.width = width;
+    this.height = height;
+    if (IGraphs.container) {
+        this.chart = IGraphs.container.append("svg:svg")
+            .attr("class", "graph")
+            .attr("width", width)
+            .attr("height", height);
+        this.chart.append("svg:text")
+            .attr("class", "title")
+            .attr("text-anchor", "middle")
+            .attr("transform", "translate(" + width/2 + "," + 20 + ")")
+            .text(title);
+    }
 };
 
 IGraphs.Graph.prototype = {
@@ -42,24 +51,16 @@ IGraphs.Graph.prototype = {
  * PieChart object
  */
 IGraphs.PieChart = function(title, width, height, radius) {
-    IGraphs.Graph.call(this, title);
-    this.layout = d3.layout.pie();
-    this.colour = d3.scale.category20();
-    this.chart = IGraphs.container.append("svg:svg")
-        .attr("class", "graph")
-        .attr("width", width)
-        .attr("height", height);
-    this.arcs = this.chart.append("svg:g")
-        .attr("class", "arc")
-        .attr("transform", "translate(" + (width/2) + "," + (height/2) + ")");
-    this.labels = this.chart.append("svg:g")
-        .attr("class", "labels")
+    IGraphs.Graph.call(this, title, width, height);
+    this.slices = this.chart.append("svg:g")
+        .attr("class", "slices")
         .attr("transform", "translate(" + (width/2) + "," + (height/2) + ")");
     this.arc = d3.svg.arc()
         .startAngle(function(d){ return d.startAngle; })
         .endAngle(function(d){ return d.endAngle; })
-        .innerRadius(0)
+        .innerRadius(radius/2)
         .outerRadius(radius);
+    this.radius = radius;
 };
 
 IGraphs.PieChart.prototype = new IGraphs.Graph();
@@ -76,28 +77,53 @@ IGraphs.PieChart.prototype.updateData = function(table_id, key_column_index, val
     return this.data;
 };
 
-IGraphs.PieChart.prototype.pieHelper = d3.layout.pie().value(function(d) {return d.value;});
-
 IGraphs.PieChart.prototype.draw = function(element) {
+    var pieHelper = d3.layout.pie().value(function(d) {return d.value;});
+    var pieData = pieHelper(this.data);
     var total = 0;
-    var pieData = this.pieHelper(this.data);
-    function filterData(element, index, array) {
-        element.name = this.data[index].key;
-        element.value = this.data[index].value;
+    pieData.filter(function(element, index, array) {
         total += element.value;
         return (element.value > 0);
-    }
-    //console.log(pie);
-    //var data = pie.filter(filterData);
+    });
     
     if (pieData.length > 0) {
-        var getColour = d3.scale.category20();
-        var arcs = this.arcs.selectAll("path").data(pieData);
-        arcs.enter().append("svg:path")
+        // assign local pointers to instance attributes
+        var arc = this.arc;
+        var data = this.data;
+        var radius = this.radius;
+        // helper functions
+        var alignText = function(d) {
+            return (d.startAngle + d.endAngle)/2 > Math.PI ? "end" : "start";
+        };
+        var calcOffset = function(d) {
+            var c = arc.centroid(d);
+            var h = Math.sqrt(c[0]*c[0] + c[1]*c[1]);
+            var r = radius + 16;
+            return "translate(" + c[0]/h * r + "," + c[1]/h * r + ")";
+        };
+        var getColour = d3.scale.category20();        
+        // create the svg elements
+        this.chart.select(".title")
+            .attr("transform", "translate(" + this.width/2 + "," + this.height/2 + ")");
+        var slices = this.slices.selectAll("path").data(pieData);
+        slices.enter().append("svg:g")
+            .attr("class", "slice");
+        slices.append("svg:path")
             .attr("stroke", "white")
             .attr("stroke-width", 0.5)
             .attr("fill", function(d, i) { return getColour(i); })
             .attr("d", this.arc);
-        arcs.exit().remove();
+        slices.append("svg:text")
+            .attr("class", "label")
+            .attr("text-anchor", alignText)
+            .attr("transform", calcOffset)
+            .text(function(d, i) { return data[i].key; });
+        slices.append("svg:text")
+            .attr("class", "percentage")
+            .attr("dy", "1.5em")
+            .attr("text-anchor", alignText)
+            .attr("transform", calcOffset)
+            .text(function(d, i) { return (data[i].value * 100.0 / total + "%"); });
+        slices.exit().remove();
     }
 };
